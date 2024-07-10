45b79440c13d59e2d24431eecb924afa0be149c4
seanchien4 <schien@tamu.edu>
2024-07-10 13:44:45 -0500

    Tii

--- figure/TII_plot.R
+++ figure/TII_plot.R
@@ -7,7 +7,7 @@ datMesquite<- read.csv("../data/TII_Mesquite.csv")
 # plot
 plot(x = (1:length(datMesquite$ID)),y = datMesquite$TII[order(datMesquite$TII)],
      xlab = "Taxa", ylab = "Taxonomic Instability Index")
-th <- 184000
+th <- 200000
 abline(h = th, col = 'red')
 rogue_list <- datMesquite$ID[datMesquite$TII > th]
 length(rogue_list)/length(datMesquite$ID)
