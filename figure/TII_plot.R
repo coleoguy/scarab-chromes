ab848312cc2b70d9bb84088e247baaa9ff3066a8
seanchien4 <schien@tamu.edu>
2024-07-10 13:32:18 -0500

    TII plot

---
+++ figure/TII_plot.R
@@ -0,0 +1,15 @@
+# Sean Chien
+# TII plot
+library(seqinr)
+###TII by Mesquite
+## bs500 
+datMesquite<- read.csv("../data/TII_Mesquite.csv")
+# plot
+plot(x = (1:length(datMesquite$ID)),y = datMesquite$TII[order(datMesquite$TII)],
+     xlab = "Taxa", ylab = "Taxonomic Instability Index")
+th <- 184000
+abline(h = th, col = 'red')
+rogue_list <- datMesquite$ID[datMesquite$TII > th]
+length(rogue_list)/length(datMesquite$ID)
+# keep the outgroup
+rogue_list <- rogue_list[!rogue_list == 'Enochrus falcarius']
