9379bebcde2815f96e8f9cf193f46f4644fc6e62
seanchien4 <schien@tamu.edu>
2025-07-06 13:22:34 -0500

    figures

--- figure/Figure_2_scs_autosome_dist.R
+++ figure/Figure_2_scs_autosome_dist.R
@@ -13,17 +13,20 @@ for (i in 1:length(type_num_dat$Family)){
 df <- df[-21,]
 cols <- viridis(3, option = 'D',alpha = 0.7, begin = 0.45)
 famcolor <- c('Scarabaeidae' = cols[1],"Lucanidae" = cols[2],"Passalidae" = cols[3])
+cex = 2
 beeswarm( df$autosome.haploid~df$SCS,
           method = c('center'),
-          cex =2, pch = 16, spacing = 0.6,
+          cex = cex , pch = 16, spacing = 0.6,
           xlab = 'Sex Chromosome System', ylab = 'Haploid Autosome Number', pwcol = famcolor[df$Family],
           corral = c("random"),
           cex.lab = 1.2
 )
-points(0.5,21, col = cols[1], pch = 16)
+cex = 1.5
+cols <- viridis(3, option = 'D', begin = 0.45)
+points(0.5,21, col = cols[1], pch = 16, cex = cex )
 text(0.5,21, pos = 4, labels = 'Scarabaeidae')
-points(0.5,20, col = cols[2], pch = 16)
+points(0.5,20, col = cols[2], pch = 16, cex = cex )
 text(0.5,20, pos = 4, labels = 'Lucanidae')
-points(0.5,19, col = cols[3], pch = 16)
+points(0.5,19, col = cols[3], pch = 16, cex = cex )
 text(0.5,19, pos = 4, labels = 'Passalidae')
 # save PDF 6x6
--- figure/Figure_4_phylo-chrom-heatmap.R
+++ figure/Figure_4_phylo-chrom-heatmap.R
@@ -5,8 +5,9 @@ library(viridis)
 trees <- read.tree('../data/final100trees')
 chrom <- read.csv('../data/SpeciesChromList.csv')
 # random pick one tree
-set.seed(6)
+set.seed(4)
 tree <- trees[[sample(100,1)]]
+# trimming genus level
 genus <- c()
 for (i in 1:length(tree$tip.label)){
   # full name
@@ -30,8 +31,21 @@ trim.genus.tree <- keep.tip(tree, tip = tips)
 # change tip label only genus level
 for (i in 1:length(trim.genus.tree$tip.label)){
   trim.genus.tree$tip.label[i] <- sub("_.*", "", trim.genus.tree$tip.label[i])
-    
+  
 }
+
+fam <- c()
+for (i in 1:length(trim.genus.tree$tip.label)){
+  if (trim.genus.tree$tip.label[i] %in% chrom$Genus){
+    fam <- c(fam, chrom$Family[which(trim.genus.tree$tip.label[i] == chrom$Genus)][1])
+  }
+}
+
+# reorder tip 
+tip_order <- trim.genus.tree$tip.label[order(fam)]
+tree_ordered <- rotateConstr(trim.genus.tree, tip_order)
+
+
 # creating data table
 table <- matrix(data = 0, nrow = length(tips), ncol = (range(chrom$male.haploid)[2]-range(chrom$male.haploid)[1]+1))
 table <- data.frame(table)
@@ -48,13 +62,30 @@ for (i in 1:length(genus)){
 table <- table[,-c(19,18)]
 cols <- viridis(40, direction = -1, option = "F")
 cols[1] <- 'white'
-phylo.heatmap(trim.genus.tree, table, fsiz = c(0.0022,0.5,0.5),
+phylo.heatmap(tree_ordered, table, fsiz = c(0.0022,0.5,0.5),
               split=c(0.7,0.3),
               legend = T,labels = T,
               legend.pos = c(0.02, 0.01),
               pts = F, lwd = 0.5,
-              #colors = heat.colors(n= 300)[300:1])
               colors = cols)
+famcols <- viridis(3, option = 'D', begin = 0.45)
+fam <- c(rep("Lucanidae",8),rep("Passalidae",12),rep("Scarabaeidae",93))
+fam <- gsub("Scarabaeidae", famcols[1], fam)
+fam <- gsub("Passalidae", famcols[2], fam)
+fam <- gsub("Lucanidae", famcols[3],fam)
+# Add colored tip labels manually
+tiplabels(pch = 19, col = fam, cex = 0.5, adj = 0.5)
+x = -0.3
+y = -0.015
+s = 0.035
+points(x,y, pch = 19, col = famcols[1])
+text(x,y, label = "Scarabaeidae", pos =4)
+points(x,y-s,pch = 19, col = famcols[2])
+text(x,y-s, label = "Passalidae", pos =4)
+points(x,y-2*s,pch = 19, col = famcols[3])
+text(x,y-2*s, label = "Lucanidae", pos =4)
+
+
 # draw grid line
 lwd = 0.05
 grid.col <- 'gray87'
@@ -68,8 +99,8 @@ x1 = 0.501
 # segments(x0 = 0.5, y0 = y0, x1 = 1.143, y1 = y0, lwd = lwd, col = grid.col)
 # segments(x0 = 0.5, y0 = y1, x1 = 1.143, y1 = y1, lwd = lwd, col = grid.col)
 
-for (i in 1:(length(trim.genus.tree$tip.label)+1)){
-  z = seq(from = y0, to = y1, length.out = length(trim.genus.tree$tip.label)+1)[i]
+for (i in 1:(length(trim.genus.tree_ordered$tip.label)+1)){
+  z = seq(from = y0, to = y1, length.out = length(trim.genus.tree_ordered$tip.label)+1)[i]
   segments(x0 = x1, y0 = z, x1 = x0, y1 = z, lwd = lwd, col = grid.col)
 }
 
@@ -78,3 +109,4 @@ for (i in 1:(ncol(table)+1)){
   segments(x0 = z, y0 = y0, x1 = z, y1 = y1, lwd = lwd,col = grid.col)
 }
 # export 600x600
+
