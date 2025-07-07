f2d907d2be741ad19560b8e501cd033cb3d460d3
seanchien4 <schien@tamu.edu>
2025-07-07 11:01:24 -0500

    figure

--- figure/Figure_S2_all_fusion-fission.R
+++ figure/Figure_S2_all_fusion-fission.R
@@ -17,21 +17,22 @@ post.chrom.number.results <- chrom.number.results[chrom.number.results$i == c(51
 mean(post.chrom.number.results$asc1)
 mean(post.chrom.number.results$desc1)
 cols <- viridis(2, option = 'D',alpha = 0.7, begin = 0.45)
+hpdcols <- viridis(2, option = 'D', begin = 0.45)
 plot(density((post.chrom.number.results$asc1)),main ='',xlab='Rate(/MY)',
      ylim =c(-10,300), xlim=c(0.002,0.015),)
 polygon(density(post.chrom.number.results$asc1),col=cols[1])
 hpd <- HPDinterval(as.mcmc(post.chrom.number.results$asc))
 y <- -5
-lines(y=c(y,y), x=hpd[1:2], lwd=2,col=cols[1])
+lines(y=c(y,y), x=hpd[1:2], lwd=2,col=hpdcols[1])
 # desc
 lines(density(post.chrom.number.results$desc1))
 polygon(density(post.chrom.number.results$desc1),col=cols[2])
 hpd <- HPDinterval(as.mcmc(post.chrom.number.results$desc))
 y <- -10
-lines(y=c(y,y), x=hpd[1:2], lwd=2,col=cols[2])
+lines(y=c(y,y), x=hpd[1:2], lwd=2,col=hpdcols[2])
 x = 0.0133
-points(x, 300, pch = 16, col = cols[1])
-text(x, 300, pos = 4, labels = 'Fission', cex = 0.9)
-points(x, 285, pch = 16, col = cols[2])
-text(x, 285, pos = 4, labels = 'Fussion', cex = 0.9)
+points(x, 300, pch = 16, col = hpdcols[1], cex = 1.2)
+text(x, 300, pos = 4, labels = 'Fission', cex = 1)
+points(x, 285, pch = 16, col = hpdcols[2], cex = 1.2)
+text(x, 285, pos = 4, labels = 'Fussion', cex = 1)
 # PDF 6x6
