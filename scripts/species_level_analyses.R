677215123177f579fc5c9cadf6593ba89d7f3398
seanchien4 <schien@tamu.edu>
2026-01-14 12:44:16 -0600

    revision

--- scripts/species_level_analyses.R
+++ scripts/species_level_analyses.R
@@ -273,6 +273,9 @@ for(i in 1:length(trees)){
 # random pick one
 # for plotting I chose 37 so keep it consistent
 tree <- trees[[37]]
+
+for (t in 1:length(trees)){
+  tree <- trees[[t]]
 #making Q matrix
 rng <- range(chrom$Chroms)
 rng.len <- rng[2]-rng[1]+1
@@ -377,82 +380,132 @@ for(i in 1:nrow(x)){
 }
 colnames(x) <- 1:(rng.len*3)
 sim <- 100
+test <- make.simmap2(tree, x = x, model = chrom.mat, pi = 'fitzjohn', nsim = sim,rejmax = 1000000,rejint = 100000, monitor = T )
+saveRDS(test, file = paste0('../results/simmap_species_level/simmap_',t,'.rds'))
+}
+
 # test <- make.simmap2(tree, x = x, model = chrom.mat, pi = 'fitzjohn', nsim = sim,rejmax = 1000000,rejint = 100000, monitor = T )
 # saveRDS(test, file = '../results/simmap_species_level.rds')
 test <- readRDS('../results/simmap_species_level.rds')
+
+
+
 #
-counts <- describe.simmap2(test)$count
-## AA fusion (desc)
-# column that are fusions
-need.col <- c()
-for (i in 2:rng.len){
-  need.col <- c(need.col,paste(i,i-1, sep = ','))
+exp <- matrix(NA,nrow = 100, ncol = 100)
+obs <-  matrix(NA,nrow = 100, ncol = 100)
+for (t in 1:length(trees)){
+  print(paste0('Tree: ',t))
+  test <- readRDS(paste0('../results/simmap_species_level/simmap_',t,'.rds'))
+  counts <- describe.simmap2(test)$count
+  ## AA fusion (desc)
+  # column that are fusions
+  need.col <- c()
+  for (i in 2:rng.len){
+    need.col <- c(need.col,paste(i,i-1, sep = ','))
+  }
+  for (i in 19:(rng.len*2)){
+    need.col <- c(need.col,paste(i,i-1, sep = ','))
+  }
+  for (i in 36:(rng.len*3)){
+    need.col <- c(need.col,paste(i,i-1, sep = ','))
+  }
+  AAfusion <- rowSums(counts[,which(colnames(counts) %in% need.col,T)])
+  ## SAfusion
+  need.col <- c()
+  for (i in (rng.len+1+1):(rng.len*2)){
+    need.col <- c(need.col,paste(i,i-18, sep = ','))
+  }
+  for (i in 36:51){
+    need.col <- c(need.col,paste(i,i-35,sep = ','))
+  }
+  for (i in 19:34){
+    need.col <- c(need.col,paste(i,i+16, sep = ','))
+  }
+  SAfusion <- rowSums(counts[,which(colnames(counts) %in% need.col,T)])
+  totalfusion <- rowSums(counts[,colnames(counts)])
+  obspropSA <- SAfusion / (SAfusion + AAfusion)
+  expSA <- c()
+  for(i in 1:100){
+    print(i)
+    times <- describe.simmap(test[[i]])$times[2, ]
+    expSA[i] <- sum(Pfsa(Da =  6, scs = "XY") * times[c(1, 35)],
+                    Pfsa(Da =  8, scs = "XY") * times[c(2, 36)],
+                    Pfsa(Da = 10, scs = "XY") * times[c(3, 37)],
+                    Pfsa(Da = 12, scs = "XY") * times[c(4, 38)],
+                    Pfsa(Da = 14, scs = "XY") * times[c(5, 39)],
+                    Pfsa(Da = 16, scs = "XY") * times[c(6, 40)],
+                    Pfsa(Da = 18, scs = "XY") * times[c(7, 41)],
+                    Pfsa(Da = 20, scs = "XY") * times[c(8, 42)],
+                    Pfsa(Da = 22, scs = "XY") * times[c(9, 43)],
+                    Pfsa(Da = 24, scs = "XY") * times[c(10, 44)],
+                    Pfsa(Da = 26, scs = "XY") * times[c(11, 45)],
+                    Pfsa(Da = 28, scs = "XY") * times[c(12, 46)],
+                    Pfsa(Da = 30, scs = "XY") * times[c(13, 47)],
+                    Pfsa(Da = 32, scs = "XY") * times[c(14, 48)],
+                    Pfsa(Da = 36, scs = "XY") * times[c(15, 50)],
+                    Pfsa(Da = 38, scs = "XY") * times[c(16, 51)],
+                    #XO
+                    Pfsa(Da =  7, scs = "XO") * times[18],
+                    Pfsa(Da =  9, scs = "XO") * times[19],
+                    Pfsa(Da = 11, scs = "XO") * times[20],
+                    Pfsa(Da = 13, scs = "XO") * times[21],
+                    Pfsa(Da = 15, scs = "XO") * times[22],
+                    Pfsa(Da = 17, scs = "XO") * times[23],
+                    Pfsa(Da = 19, scs = "XO") * times[24],
+                    Pfsa(Da = 21, scs = "XO") * times[25],
+                    Pfsa(Da = 23, scs = "XO") * times[26],
+                    Pfsa(Da = 25, scs = "XO") * times[27],
+                    Pfsa(Da = 27, scs = "XO") * times[28],
+                    Pfsa(Da = 29, scs = "XO") * times[29],
+                    Pfsa(Da = 31, scs = "XO") * times[30],
+                    Pfsa(Da = 33, scs = "XO") * times[31],
+                    Pfsa(Da = 35, scs = "XO") * times[32],
+                    Pfsa(Da = 37, scs = "XO") * times[33],
+                    Pfsa(Da = 39, scs = "XO") * times[34]
+    )
+  }
+  # save the data
+  exp[,t] <- expSA
+  obs[,t] <- obspropSA
 }
-for (i in 19:(rng.len*2)){
-  need.col <- c(need.col,paste(i,i-1, sep = ','))
-}
-for (i in 36:(rng.len*3)){
-  need.col <- c(need.col,paste(i,i-1, sep = ','))
-}
-AAfusion <- rowSums(counts[,which(colnames(counts) %in% need.col,T)])
-## SAfusion
-need.col <- c()
-for (i in (rng.len+1+1):(rng.len*2)){
-  need.col <- c(need.col,paste(i,i-18, sep = ','))
-}
-for (i in 36:51){
-  need.col <- c(need.col,paste(i,i-35,sep = ','))
-}
-for (i in 19:34){
-  need.col <- c(need.col,paste(i,i+16, sep = ','))
-}
-SAfusion <- rowSums(counts[,which(colnames(counts) %in% need.col,T)])
-totalfusion <- rowSums(counts[,colnames(counts)])
-obspropSA <- SAfusion / (SAfusion + AAfusion)
-expSA <- c()
-for(i in 1:100){
-  print(i)
-  times <- describe.simmap(test[[i]])$times[2, ]
-  expSA[i] <- sum(Pfsa(Da =  6, scs = "XY") * times[c(1, 35)],
-                  Pfsa(Da =  8, scs = "XY") * times[c(2, 36)],
-                  Pfsa(Da = 10, scs = "XY") * times[c(3, 37)],
-                  Pfsa(Da = 12, scs = "XY") * times[c(4, 38)],
-                  Pfsa(Da = 14, scs = "XY") * times[c(5, 39)],
-                  Pfsa(Da = 16, scs = "XY") * times[c(6, 40)],
-                  Pfsa(Da = 18, scs = "XY") * times[c(7, 41)],
-                  Pfsa(Da = 20, scs = "XY") * times[c(8, 42)],
-                  Pfsa(Da = 22, scs = "XY") * times[c(9, 43)],
-                  Pfsa(Da = 24, scs = "XY") * times[c(10, 44)],
-                  Pfsa(Da = 26, scs = "XY") * times[c(11, 45)],
-                  Pfsa(Da = 28, scs = "XY") * times[c(12, 46)],
-                  Pfsa(Da = 30, scs = "XY") * times[c(13, 47)],
-                  Pfsa(Da = 32, scs = "XY") * times[c(14, 48)],
-                  Pfsa(Da = 36, scs = "XY") * times[c(15, 50)],
-                  Pfsa(Da = 38, scs = "XY") * times[c(16, 51)],
-                  #XO
-                  Pfsa(Da =  7, scs = "XO") * times[18],
-                  Pfsa(Da =  9, scs = "XO") * times[19],
-                  Pfsa(Da = 11, scs = "XO") * times[20],
-                  Pfsa(Da = 13, scs = "XO") * times[21],
-                  Pfsa(Da = 15, scs = "XO") * times[22],
-                  Pfsa(Da = 17, scs = "XO") * times[23],
-                  Pfsa(Da = 19, scs = "XO") * times[24],
-                  Pfsa(Da = 21, scs = "XO") * times[25],
-                  Pfsa(Da = 23, scs = "XO") * times[26],
-                  Pfsa(Da = 25, scs = "XO") * times[27],
-                  Pfsa(Da = 27, scs = "XO") * times[28],
-                  Pfsa(Da = 29, scs = "XO") * times[29],
-                  Pfsa(Da = 31, scs = "XO") * times[30],
-                  Pfsa(Da = 33, scs = "XO") * times[31],
-                  Pfsa(Da = 35, scs = "XO") * times[32],
-                  Pfsa(Da = 37, scs = "XO") * times[33],
-                  Pfsa(Da = 39, scs = "XO") * times[34]
-  )
-}
-mean(expSA)
-mean(obspropSA)
+row.names(exp) <- c(1:100)
+row.names(obs) <- c(1:100)
+colnames(exp) <- paste0("tree", 1:100)
+colnames(obs) <- paste0("tree", 1:100)
+# write.csv(exp, file = '../results/simmap_species_level/exp.csv')
+# write.csv(obs, file = '../results/simmap_species_level/obs.csv')
 # plot
 cols <- viridis(2, begin = 0.5, alpha = 0.65)
+exp <- read.csv('../results/simmap/exp.csv', row.names = 1)
+obs <- read.csv('../results/simmap/obs.csv', row.names = 1)
+par(
+  mfrow = c(5, 5),
+  mar = c(2, 2, 1.5, 0.5)
+)
+exceed <- c()
+for (i in 1:100){
+  plot(density(exp[,i], bw = .01),
+       xlim = c(.05, 0.9), main = "",
+       ylim = c(-0.5, 40),
+       xlab = "Proportion of Sex-Autosome Fusion")
+  polygon(density(exp[,i], bw = .01),
+          col = cols[1])
+  lines(density(obs[,i]))
+  polygon(density(obs[,i]),
+          col = cols[2])
+  exphpd <- HPDinterval(as.mcmc(exp[,i]))
+  obshpd <- HPDinterval(as.mcmc(obs[,i]))
+  exp_min <- min(exphpd)
+  exp_max <- max(exphpd)
+  obs_min <- min(obshpd)
+  obs_max <- max(obshpd)
+  # overlap and observe mean < exp mean
+  no_overlap_flag <- exp_max < obs_min || obs_max < exp_min
+  mean_less_flag <- mean(exphpd, na.rm = TRUE) < mean(obshpd, na.rm = TRUE)
+  exceed <- c(exceed,no_overlap_flag && mean_less_flag)
+}
+sum(exceed)
+
 hpdcols <- viridis(2, begin = 0.5)
 plot(density(expSA, bw = .01),
      xlim = c(.15, 0.65), main = "",
@@ -475,37 +528,3 @@ text(x=x,y=38, pos = 4, labels = "Observed", cex = 1)
 hpd <- HPDinterval(as.mcmc(obspropSA))
 lines(y=c(y,y), x=hpd[1:2], lwd = lwd,col=hpdcols[2])
 # save as PDF 6x6
-
-
-## simmap plot
-library(fields)
-# NeoXY, XO, XY
-cols <- setNames(c(colorRampPalette(c("firebrick1", "firebrick4"))(17),
-                   colorRampPalette(c("green", "green4"))(17),
-                   colorRampPalette(c("dodgerblue", "dodgerblue4"))(17)), 
-                 c(1:51))
-plotSimmap(test[[1]], type = 'fan', fsize = 0.03, colors = cols)
-sexmode <- as.factor(setNames(chrom$SCS, chrom$Species))
-sexmode <- to.matrix(sexmode, levels(sexmode))
-sexmode <- sexmode[test[[1]]$tip.label,]
-tiplabels(pie = sexmode, piecol = palette()[c(2,3,4)], cex = 0.15)
-# Create a color palette
-color_pal <- colorRampPalette(c("firebrick1", "firebrick4"))(17)
-data_matrix <- matrix(1:17, nrow = 17, ncol = 1)
-image.plot(z = data_matrix, col = color_pal, axes = FALSE, legend.only = TRUE,
-           legend.width = 1,
-           legend.shrink = 1, legend.mar = 3)
-
-color_pal <- colorRampPalette(c("green", "green4"))(17)
-data_matrix <- matrix(1:17, nrow = 17, ncol = 1)
-image.plot(z = data_matrix, col = color_pal, axes = FALSE, legend.only = TRUE,
-           legend.width = 1,
-           legend.shrink = 1, add = TRUE, legend.mar = 6,
-           axis.args = list(labels = FALSE, tick = FALSE))
-
-color_pal <- colorRampPalette(c("dodgerblue", "dodgerblue4"))(17)
-data_matrix <- matrix(1:17, nrow = 17, ncol = 1)
-image.plot(z = data_matrix, col = color_pal, axes = FALSE, legend.only = TRUE,
-           legend.width = 1,
-           legend.shrink = 1, add = TRUE, legend.mar = 9,
-           axis.args = list(labels = FALSE, tick = FALSE))
